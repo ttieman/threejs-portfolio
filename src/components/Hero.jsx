33308c48180c8fdf45d13cf46bccc7e0cff5ac89
ttieman <tiemantanner@gmail.com>
2023-03-15 18:27:29 -0700

    got the 3d model rendering on the screen within the hero element

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,23 +11,24 @@ const Hero = () => {
       flex-row items-start gap-5`}>
         <div className='flex flex-col 
         justify-center items-center mt-5'>
-          <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
+          <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 
           violet-gradient' />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}
           text-white`}>Hi, I'm &nbsp;
-          <span className='text-[#915eff]'>Tanner</span></h1>
+            <span className='text-[#915eff]'>Tanner</span></h1>
           <p className={`${styles.heroSubText} mt-2
           text-white-100`}>I am the &nbsp;
-          <span className='text-[#915eff]'>Full Stack Developer</span> behind &nbsp;
-          <span className='text-[#915eff]'>ODD ONE DESIGNS</span> Where I use technology to infuse
-          whimsy, and eccentricity to create indelible digital solutions. With a focus on transforming 
-          the tech landscape, I blend innovation, expertise, and a touch of absurdity to create standout 
-          experiences that defy convention and captivate audiences.</p>
+            <span className='text-[#915eff]'>Full Stack Developer</span> <br className='sm:block hidden'/> behind &nbsp; 
+            <span className='text-[#915eff]'>ODD ONE DESIGNS</span> Where I use technology to infuse
+            whimsy, and eccentricity to create indelible digital solutions. With a focus on transforming
+            the tech landscape, I blend innovation, expertise, and a touch of absurdity to create standout
+            experiences that defy convention and captivate audiences.</p>
         </div>
       </div>
+        <ComputersCanvas />
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { CanvasLoader } from "../Loader";
+
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh> {/* everything should remain within a mesh tag */}
+      <hemisphereLight intensity={0.15} // adds lighting the the mesh
+        groundColor="black" />
+      <pointLight //adds directional light to the scene
+        intensity={1} />
+      <primitive //imports the mesh
+        object={computer.scene} //selects the object wanting to be rendered 
+        scale={0.55} //scales the object
+        position={[0, -3.3, -1]}
+        /> 
+    </mesh>
   )
 }
 
-export default Computers
+const ComputersCanvas = () => {
+  return (
+    <Canvas
+      frameloop='demand' //animation frames 
+      shadows  // implements shadows 
+      camera={{ position: [20, 3, 5], fov: 25 }} /* imports camera and camera settings*/
+      gl={{ preserveDrawingBuffer: true }} // ensures proper rendering of the model
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false}  // effects the canvas controls
+          maxPolarAngle={Math.PI / 2} // adds limitaions for the polar angles 
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputersCanvas
